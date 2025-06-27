598de282d0f76d301e735e944e3b707897bd0839
Math012 <math111.0811@gmail.com>
2025-06-27 19:06:07 -0300

    Adicionando método para retornar mensagens dentro de um periado de tempo e modificando o tipo do campo dataHoraEnvio de Date para LocalDateTime

--- src/main/java/com/luizalebs/comunicacao_api/infraestructure/repositories/ComunicacaoRepository.java
+++ src/main/java/com/luizalebs/comunicacao_api/infraestructure/repositories/ComunicacaoRepository.java
@@ -1,9 +1,15 @@
 package com.luizalebs.comunicacao_api.infraestructure.repositories;
 
 import com.luizalebs.comunicacao_api.infraestructure.entities.ComunicacaoEntity;
+import com.luizalebs.comunicacao_api.infraestructure.enums.StatusEnvioEnum;
 import org.springframework.data.repository.CrudRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ComunicacaoRepository extends CrudRepository<ComunicacaoEntity, Long> {
 
     ComunicacaoEntity findByEmailDestinatario(String nomeDestinatario);
+    List<ComunicacaoEntity> findByDataHoraenvioBetweenAndStatusEnvio(LocalDateTime horaAtual, LocalDateTime horaFutura, StatusEnvioEnum statusEnvioEnum);
+
 }
