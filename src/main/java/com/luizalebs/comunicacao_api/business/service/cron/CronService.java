248a2e28a29366ac2b1a1dada00c1dcbce977661
Math012 <math111.0811@gmail.com>
2025-07-03 21:21:38 -0300

    Testes finalizados para a classe CronService

--- src/main/java/com/luizalebs/comunicacao_api/business/service/cron/CronService.java
+++ src/main/java/com/luizalebs/comunicacao_api/business/service/cron/CronService.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,11 +18,13 @@ public class CronService {
 
     private final ComunicacaoService comunicacaoService;
     private final EmailService emailService;
+    private final Clock clock;
+
 
     @Scheduled(cron = "${cron.horario}")
     public void enviarMensagem(){
-        LocalDateTime horaInicial = LocalDateTime.now();
-        LocalDateTime horaFinal = LocalDateTime.now().plusHours(1);
+        LocalDateTime horaInicial = LocalDateTime.now(clock);
+        LocalDateTime horaFinal = LocalDateTime.now(clock).plusHours(1);
         List<ComunicacaoOutDTO> listaMensagens = comunicacaoService.buscarMensagemPorPeriado(horaInicial,horaFinal);
         listaMensagens.forEach(mensagem ->{
             emailService.enviarEmail(mensagem);
