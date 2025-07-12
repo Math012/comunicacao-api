97f016e6e3f1341415dc31d71ee4ba34f623307c
Math012 <math111.0811@gmail.com>
2025-07-12 06:30:12 -0300

    Adicionando novas conversões para o ComunicadoMapper

--- target/generated-sources/annotations/com/luizalebs/comunicacao_api/business/converter/ComunicadoMapperImpl.java
+++ target/generated-sources/annotations/com/luizalebs/comunicacao_api/business/converter/ComunicadoMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-07-11T18:04:49-0300",
+    date = "2025-07-12T06:23:35-0300",
     comments = "version: 1.5.4.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
@@ -55,6 +55,46 @@ public class ComunicadoMapperImpl implements ComunicadoMapper {
         comunicacaoOutDTO.statusEnvio( comunicacaoEntity.getStatusEnvio() );
 
         return comunicacaoOutDTO.build();
+    }
+
+    @Override
+    public ComunicacaoOutDTO paraComunicadoOutDTOFromComunicacaoInDTO(ComunicacaoInDTO comunicacaoInDTO) {
+        if ( comunicacaoInDTO == null ) {
+            return null;
+        }
+
+        ComunicacaoOutDTO.ComunicacaoOutDTOBuilder comunicacaoOutDTO = ComunicacaoOutDTO.builder();
+
+        comunicacaoOutDTO.dataHoraEnvio( comunicacaoInDTO.getDataHoraEnvio() );
+        comunicacaoOutDTO.nomeDestinatario( comunicacaoInDTO.getNomeDestinatario() );
+        comunicacaoOutDTO.emailDestinatario( comunicacaoInDTO.getEmailDestinatario() );
+        comunicacaoOutDTO.telefoneDestinatario( comunicacaoInDTO.getTelefoneDestinatario() );
+        comunicacaoOutDTO.mensagem( comunicacaoInDTO.getMensagem() );
+        comunicacaoOutDTO.nomeRemetente( comunicacaoInDTO.getNomeRemetente() );
+        comunicacaoOutDTO.modoDeEnvio( comunicacaoInDTO.getModoDeEnvio() );
+        comunicacaoOutDTO.statusEnvio( comunicacaoInDTO.getStatusEnvio() );
+
+        return comunicacaoOutDTO.build();
+    }
+
+    @Override
+    public ComunicacaoInDTO paraComunicadoInDTOFromComunicacaoOutDTO(ComunicacaoOutDTO comunicacaoOutDTO) {
+        if ( comunicacaoOutDTO == null ) {
+            return null;
+        }
+
+        ComunicacaoInDTO.ComunicacaoInDTOBuilder comunicacaoInDTO = ComunicacaoInDTO.builder();
+
+        comunicacaoInDTO.dataHoraEnvio( comunicacaoOutDTO.getDataHoraEnvio() );
+        comunicacaoInDTO.nomeDestinatario( comunicacaoOutDTO.getNomeDestinatario() );
+        comunicacaoInDTO.emailDestinatario( comunicacaoOutDTO.getEmailDestinatario() );
+        comunicacaoInDTO.telefoneDestinatario( comunicacaoOutDTO.getTelefoneDestinatario() );
+        comunicacaoInDTO.mensagem( comunicacaoOutDTO.getMensagem() );
+        comunicacaoInDTO.nomeRemetente( comunicacaoOutDTO.getNomeRemetente() );
+        comunicacaoInDTO.modoDeEnvio( comunicacaoOutDTO.getModoDeEnvio() );
+        comunicacaoInDTO.statusEnvio( comunicacaoOutDTO.getStatusEnvio() );
+
+        return comunicacaoInDTO.build();
     }
 
     @Override
